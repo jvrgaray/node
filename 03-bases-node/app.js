e9730e55f862ff2a94f970f67b75579ebd1f6056
Javier Garay <javier.garay@2brains.cl>
2020-02-20 17:25:58 -0300

    31. Recibir información desde linea de comando

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,6 +1,11 @@
 const { crearArchivo } = require("./multiplicar/multiplicar");
 
-let base = 1;
+//let base = 1;
+let argv = process.argv;
+let parametro = argv[2];
+let base = parametro.split('=')[1];
+//console.log(process.argv);
+console.log(base);
 
 crearArchivo(base)
 .then (archivo => console.log(`Archivo creado: ${archivo}`))
