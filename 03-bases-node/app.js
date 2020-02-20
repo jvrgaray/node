857fff1532273118b45237d46eff17c2d0f2acb6
Javier Garay <javier.garay@2brains.cl>
2020-02-20 16:02:28 -0300

    29. Requerir paquetes - required

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,4 +1,15 @@
-let base = 2;
+const fs = require('fs');
+
+let base = 5;
+let data1 = '';
+
 for (let i = 1; i < 11; i++) {
-  console.log(`${base} x ${i} = ${base * i}`);
+  data1 += `${base} x ${i} = ${base * i} \n`;
 }
+
+
+const data = new Uint8Array(Buffer.from(data1));
+fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
