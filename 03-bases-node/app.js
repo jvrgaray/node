d54af6a9520dc975fb7094c704705a43574824b3
Javier Garay <javier.garay@2brains.cl>
2020-03-05 12:35:10 -0300

    Yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,12 +1,27 @@
+const argv = require('yargs')
+                    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+                        base: {
+                            demand: true,
+                            alias: 'b'
+                        },
+                        limite: {
+                            alias: 'l',
+                            default: 10
+                        }
+                    })
+                    .help()
+                    .argv;
 const { crearArchivo } = require("./multiplicar/multiplicar");
 
 //let base = 1;
-let argv = process.argv;
-let parametro = argv[2];
+let argv2 = process.argv;
+console.log(argv.base);
+console.log(argv.limite);
+/*let parametro = argv[2];
 let base = parametro.split('=')[1];
 //console.log(process.argv);
 console.log(base);
 
 crearArchivo(base)
 .then (archivo => console.log(`Archivo creado: ${archivo}`))
-.catch(err => console.log(err));
+.catch(err => console.log(err));*/
